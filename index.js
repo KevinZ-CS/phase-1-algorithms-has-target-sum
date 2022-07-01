516944d2772c9cbf773f893f9a91c4b3ac476e58
Kevin Zheng <kevzheng27@gmail.com>
2022-07-01 17:17:53 -0400

    commit tree 1b60e8a9b4a3a7ead8adf80f75610f7c28759dc5

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j <array.length; j++)
+      if (array[i] + array[j] === target) {
+        return true
+      } 
+  }
+  return false
 }
 
+
 /* 
-  Write the Big O time complexity of your function here
+Runtime: O(n^2)
+Space: O(n)
 */
 
 /* 
-  Add your pseudocode here
+  take the first element of the array and sum it with every subsequent elements
+  if the sum adds up to the target value return the sum
+  if not move on to the second element and repeat the same steps etc.
 */
 
 /*
-  Add written explanation of your solution here
+  the function takes an array and starts iterating through the array at index 0
+  with each iteration of each element a second iteration is created which essentially sums every subsequent element
+  with the the current number from the first iteration until it sums to the target number
+  the moment the target number is discovered the boolean value true will be returned thus breaking out of the loop
+  and the entire hasTargetSum function
+  if the target number is not found then the hasTargetSum function returns false
+
 */
 
 // You can run `node index.js` to view these console logs
